666ae0a05b480530f2a23596767f92ad64cb1b77
LeVDuan <duan.lv0308@gmail.com>
2024-06-23 00:08:32 +0700

    Fixed bug related to calling api from different domains.

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ import deviceRoutes from './routes/devices.js'
 import logsRoutes from './routes/logs.js'
 
 const app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 app.use(cors());
 app.use(express.json());
 
